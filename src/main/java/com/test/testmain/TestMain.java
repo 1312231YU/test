89d6a0658d0b9a23566ecc975efd0506e2c21d65
yumm <2959329599@qq.com>
2021-11-14 17:17:57 +0800

    Initial commit

---
+++ src/main/java/com/test/testmain/TestMain.java
@@ -0,0 +1,24 @@
+package com.test.testmain;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @Description TODO
+ * @Author yumm
+ * @Date 2021/10/12 20:08
+ * @Version 1.0
+ **/
+public class TestMain {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(TestMain.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("开始执行了");
+
+        LOGGER.info("结束了");
+    }
+
+}
